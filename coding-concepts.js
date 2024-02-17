80c63226b8017cf9aa4c7895c67f94e1d11cfbc0
smorgannicole <morgann.smithh@yahoo.com>
2024-02-16 17:25:24 -0800

    unit 2 assessment ✅

--- coding-concepts.js
+++ coding-concepts.js
@@ -9,28 +9,28 @@
 // --------------------1) What will this log?
 
 const cohort = "Alpha 2024"
-// console.log(cohort.split(" "))
+console.log(cohort.split(" "))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [ "Alpha", "2024" ]
+// b) Verify and explain: .split() is a string method that turns a string to an array. Where the string is split depends on what is inside the quotation marks.
 
 // --------------------2) What will this log?
 
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "Hello, LEARN Student!"
+// b) Verify and explain: Oops! The console showed "undefined" because the function is missing a return statement! 😵
 
 // --------------------3) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-// console.log(onlyOdds)
+console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: [ 11, 13, 15 ]
+// b) Verify and explain: .filter() is a higher order fxn that returns a modified version of the original array based on whether or not the condition following the fat arrow is true.
 
 // --------------------4) What will this log?
 
@@ -40,10 +40,10 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "JavaScript"
+// b) Verify and explain: In this example, dot notation is used to access the "languages" property within the "myCodingSkills" object. Bracket notation is used on the "languages" property because its key-value pair is an array whose zeroth position is "JavaScript".
 
 // --------------------5) What will this log?
 
@@ -55,7 +55,7 @@ class Learn {
   }
 }
 const learnStudent = new Learn("George")
-// console.log(learnStudent)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Learn { student: "George", cohort: "Alpha", year: 2024 }
+// b) Verify and explain: Sweet! I wasn't sure if Learn came before the object or learnStudent. A new instance of an object is being created using the Learn class as a blueprint. The constructor is expecting one argument to be passed which will be the value associated with the student key.
